5e33d3d16745da9d16cede42d93bff5ac596552d
lucig4 <akostas9@gmail.com>
2019-01-13 23:07:37 +0200

    FINAL COMMIT

--- Parse.pm
+++ Parse.pm
@@ -1,1 +1,24 @@
+package Parse;
 
+use strict;
+use warnings;
+
+use XML::LibXML;
+
+
+sub fields
+{
+
+	my $filename = shift;
+
+
+	#the code blah blah blah
+
+
+	return {};
+}
+
+
+
+1;
+__END__
--- test.pl
+++ test.pl
@@ -1,1 +1,16 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+
+use Test::More tests => 2;
+
+
+
+require_ok( 'Parse' );
+
+
+can_ok( 'Parse', qw( fields ) );
+
 
